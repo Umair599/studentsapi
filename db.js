d5db6ddfa94ec09badf1c8dc9c3ea4de9d12d728
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-07 20:22:16 +0500

    db.js

---
+++ db.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import mockgoose from 'mockgoose';
+const DB_URI=dotenv.config().parsed.CONNECTION_URL;
+const Mockgoose=mockgoose.Mockgoose;
+export const connect=()=> {
+
+  return new Promise((resolve, reject) => {
+
+    if (process.env.NODE_ENV === 'test') {
+      //const Mockgoose = require('mockgoose').Mockgoose;
+      const mockgoose = new Mockgoose(mongoose);
+
+      mockgoose.prepareStorage()
+        .then(() => {
+          mongoose.connect(DB_URI,
+            { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true})
+            .then((res, err) => {
+              if (err) return reject(err);
+              resolve();
+            })
+        })
+    } else {
+        mongoose.connect(DB_URI,
+          { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology:true})
+          .then((res, err) => {
+            if (err) return reject(err);
+            resolve();
+          });
+          mongoose.set('useFindAndModify', false);
+    }
+  });
+}
+
+export const close=()=> {
+  return mongoose.disconnect();
+}
