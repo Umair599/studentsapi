4796fdd6b752174efae525f982cd9b018ba93305
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-07 13:18:31 +0500

    express Validator added

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import {body} from 'express-validator';
 import {getStudentsRecord, getStudentRecord, createStudentRecord, updateStudentRecord, deleteStudentRecord} from '../controllers/studentController.js';
 const router=express.Router();
 router.get('/una/students', getStudentsRecord);
-router.get('/una/students/:id', getStudentRecord);
-router.post('/una/student', createStudentRecord);
+router.get('/una/students/:id', getStudentRecord); 
+router.post('/una/student', [body('name').trim().isLength({min: 4}).withMessage('Name must be of 4 character'), body('email').trim().isEmail().withMessage('Please enter valid Email')], createStudentRecord);
 router.put('/una/student/:id', updateStudentRecord);
 router.delete('/una/student/:id', deleteStudentRecord);
 router.get('*', (req, res)=>{
