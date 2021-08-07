5b84eb640ff59ca51fed5351532d03801a09f2ca
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-07 20:21:52 +0500

    index

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import studentRoutes from './routes/studentRoutes.js';
+import studentRoutes from './routes/studentRoutes.js'; 
+import {connect} from './db.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(studentRoutes);
-const CONNECTION_URL='mongodb+srv://umair-khalid-599:Inspiron^6304@studentsapi.kbpf1.mongodb.net/StudentsDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT||5000;
-
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>app.listen(PORT, ()=>console.log(`Server Running on Port http://localhost:${PORT}`))).catch((error)=>console.log(`${error} Fail to connect`));
-mongoose.set('useFindAndModify', false);
+connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port http://localhost:${PORT}`);
+    });
+  });
